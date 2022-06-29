ddd14b05e1f2120198741a8e41d3325bfe17f294
Rowdy Rabouw <rowdy@double-r.nl>
2022-06-29 14:10:13 +0200

    feat: add fetch static cache

--- dist/sw.js
+++ dist/sw.js
@@ -13,7 +13,6 @@ self.addEventListener("install", (event) => {
 	console.log("%c[sw.js] Service Worker installed", "color: #FEC233");
 	event.waitUntil(
 		caches.open(STATIC_CACHE).then((cache) => {
-			console.log("created!");
 			cache.addAll(STATIC_FILES).then(() => {
 				console.log("%c[sw.js] Files added to static cache", "color: #FEC233");
 				self.skipWaiting();
@@ -26,4 +25,19 @@ self.addEventListener("activate", (event) => {
 	console.log("%c[sw.js] Service Worker activated", "color: #FEC233");
 	// ensure that the Service Worker is activated correctly (fail-safe)
 	return self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+	event.respondWith(
+		// look at all caches for a match on the key (= request)
+		caches.match(event.request).then((response) => {
+			if (response) {
+				// return from cache
+				return response;
+			} else {
+				// fetch from the server
+				return fetch(event.request);
+			}
+		})
+	);
 });
